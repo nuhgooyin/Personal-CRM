77bd03b24ab22ef198d7004ebbc782179d77b099
Dan Nguyen <60051771+nuhgooyin@users.noreply.github.com>
2024-01-11 12:18:29 -0500

    Update InstagramTracker.test.js

--- Testing/InstagramTracker.test.js
+++ Testing/InstagramTracker.test.js
@@ -4,7 +4,7 @@ import {Builder} from 'selenium-webdriver';
 
 test('check for any exceptions when logging in', async ()=> {
     const driver = new Builder().forBrowser('chrome').build();
-    const tracker = new InstagramTracker(driver, 'thomas.li9034', 'WAJK&tegw43%$y', ['fyra.finance']);
+    const tracker = new InstagramTracker(driver, 'USERNAME', 'PASSWORD', ['SOMEUSER']);
 
     for (let i = 0; i < 1; i++) {
         assert (await tracker.login());
